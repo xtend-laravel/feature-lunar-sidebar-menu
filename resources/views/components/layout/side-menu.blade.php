f4c5cecc3c84ad3f8591e130f8906a97960fad37
Adam Lee <adam@codelabx.ltd>
2023-04-21 03:04:08 +0100

    lighter sidebar

--- resources/views/components/layout/side-menu.blade.php
+++ resources/views/components/layout/side-menu.blade.php
@@ -1,6 +1,6 @@
 <div>
     <aside class="hidden h-full lg:block lg:flex-shrink-0 lg:order-first">
-        <div class="w-64 h-full overflow-y-auto bg-gray-600 border-r border-gray-100 dark:border-gray-800 dark:bg-gray-900">
+        <div class="w-64 h-full overflow-y-auto bg-gray-200 border-r border-gray-100 dark:border-gray-800 dark:bg-gray-900">
             <div class="px-2">
                 {{ $slot }}
             </div>
