bdda8da9a83af7c07a238a3cd7227590dbca5418
Adam Lee <adam@codelabx.ltd>
2023-05-25 14:35:59 +0100

    Temp solution now sidebar menu is set to final class

--- src/Menu/SidebarMenu.php
+++ src/Menu/SidebarMenu.php
@@ -4,21 +4,36 @@ namespace XtendLunar\Features\SidebarMenu\Menu;
 
 use Lunar\Hub\Facades\Menu;
 use Lunar\Hub\Menu\MenuSlot;
-use Lunar\Hub\Menu\SidebarMenu as LunarSidebarMenuMenu;
 
-class SidebarMenu extends LunarSidebarMenuMenu
+class SidebarMenu
 {
     protected MenuSlot $slot;
 
-    /**
-     * Make menu.
-     *
-     * @return void
-     */
     public static function make()
     {
         (new static())
+            ->makeTopLevel()
             ->addSections();
+    }
+
+    /**
+     * Make top level navigation.
+     *
+     * @return static
+     */
+    protected function makeTopLevel()
+    {
+        $slot = Menu::slot('sidebar');
+
+        $slot->addItem(function ($item) {
+            $item
+                ->name(__('adminhub::menu.sidebar.index'))
+                ->handle('hub.index')
+                ->route('hub.index')
+                ->icon('chart-square-bar');
+        });
+
+        return $this;
     }
 
     /**
@@ -29,6 +44,70 @@ class SidebarMenu extends LunarSidebarMenuMenu
     protected function addSections()
     {
         $slot = Menu::slot('sidebar');
+
+        $catalogueGroup = $slot
+            ->group('hub.catalogue')
+            ->name(__('adminhub::menu.sidebar.catalogue'));
+
+        $salesGroup = $slot
+            ->group('hub.sales')
+            ->name(__('adminhub::menu.sidebar.sales'));
+
+        $productGroup = $catalogueGroup
+            ->section('hub.products')
+            ->name(__('adminhub::menu.sidebar.products'))
+            ->handle('hub.products')
+            ->route('hub.products.index')
+            ->icon('shopping-bag');
+
+        $catalogueGroup
+            ->section('hub.collections')
+            ->name(__('adminhub::menu.sidebar.collections'))
+            ->handle([
+                'hub.collection-groups',
+                'hub.collections',
+            ])
+            ->route('hub.collection-groups.index')
+            ->icon('collection');
+
+        $productGroup->addItem(function ($menuItem) {
+            $menuItem
+                ->name(__('adminhub::menu.sidebar.product-types'))
+                ->handle('hub.product-types')
+                ->route('hub.product-types.index');
+        });
+
+        $productGroup->addItem(function ($menuItem) {
+            $menuItem
+                ->name(__('adminhub::menu.sidebar.brands'))
+                ->handle('hub.brands')
+                ->route('hub.brands.index');
+        });
+
+        $salesGroup->addItem(function ($menuItem) {
+            $menuItem
+                ->name(__('adminhub::menu.sidebar.orders'))
+                ->handle('hub.orders')
+                ->route('hub.orders.index')
+                ->icon('cash');
+        });
+
+        $salesGroup->addItem(function ($menuItem) {
+            $menuItem
+                ->name(__('adminhub::menu.sidebar.customers'))
+                ->handle('hub.customers')
+                ->route('hub.customers.index')
+                ->icon('users');
+        });
+
+        $salesGroup->addItem(function ($menuItem) {
+            $menuItem
+                ->name(__('adminhub::menu.sidebar.discounts'))
+                ->handle('hub.discounts')
+                ->route('hub.discounts.index')
+                ->icon('ticket');
+        });
+
         $productGroup = $slot->group('hub.catalogue')->section('hub.products');
 
         // @todo - This is a hack to clear the items collection so we can define our own items from the config file
